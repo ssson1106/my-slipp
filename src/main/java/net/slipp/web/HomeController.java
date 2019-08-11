af752c213e631b1a559062dbcbf9e31a27f2a2b0
ssson1106 <ssson1106@gmail.com>
2019-08-11 16:58:11 +0900

    홈 화면 질문 리스트 불러오기 기능 추가

--- src/main/java/net/slipp/web/HomeController.java
+++ src/main/java/net/slipp/web/HomeController.java
@@ -1,15 +1,23 @@
 package net.slipp.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import net.slipp.domain.QuestionRepostory;
+
 @Controller
 public class HomeController {
+	@Autowired
+	private QuestionRepostory questionRepostory;
 	
 	@GetMapping("/")
-	public String home() {
+	public String home(Model model) {
 		System.out.println("home");
+		model.addAttribute("questions", questionRepostory.findAll());
+		
 		return "index";
 	}
 	
